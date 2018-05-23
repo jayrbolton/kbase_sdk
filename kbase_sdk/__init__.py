9685c30be5c5e8907e81e31539789482be08046c
Jay R Bolton <jayrbolton@gmail.com>
2018-05-23 11:24:21 -0700

    Move validate_params into the param_validation package

--- kbase_sdk/__init__.py
+++ kbase_sdk/__init__.py
@@ -1,4 +1,4 @@
 from kbase_sdk.init_context import init_context
-from kbase_sdk.validate_params import validate_params
+from kbase_sdk.param_validation.validate_params import validate_params
 
 __all__ = ['init_context', 'validate_params']
